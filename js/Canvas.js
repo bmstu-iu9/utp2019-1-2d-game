59f3d65dfaa52aec7fa97c1ebfdb3c697cbecbfd
vlad333rrty <vlad333rrty90@mail.ru>
2019-07-25 00:17:47 +0300

    Refresh all

--- js/Canvas.js
+++ js/Canvas.js
@@ -2,3 +2,12 @@
 
 const canvas=document.getElementById('canvas');
 const context=canvas.getContext('2d');
+
+
+//не работает с context
+let gl;
+try{
+    gl=canvas.getContext('webgl')||canvas.getContext('experimental-webgl');
+}catch (e) {
+    alert('Error occurred initializing webgl');
+}
--- js/Entity.js
+++ js/Entity.js
@@ -4,11 +4,9 @@ class Entity{
         this.texture=new Texture(textureName);
         this.scale=scale;
         this.pos=new Vector2d(0,0);
-        this.boundingBox=new AABB(0,0);
+        this.boundingBox=new AABB(new Vector2d(0,0),new Vector2d(1,1));
     }
 
     //дописать
-    collide(){
 
-    }
 }
