34d4aef951c41d13c64ec4058dfd97017d32c715
vlad333rrty <vlad333rrty90@mail.ru>
2019-08-01 13:36:42 +0300

    #2 Input v2 released

--- js/Input.js
+++ js/Input.js
@@ -1,14 +1,14 @@
 'use strict';
 
 let keyboard={
-    up:false,
-    down:false,
-    right:false,
-    left:false,
-    keyW:false,
-    keyA:false,
-    keyS:false,
-    keyD:false
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowRight:false,
+    ArrowLeft:false,
+    KeyW:false,
+    KeyA:false,
+    KeyS:false,
+    KeyD:false
 };
 
 let mouse={
@@ -17,76 +17,28 @@ let mouse={
     clickPosition:new Vector2d()
 };
 
-addEventListener('keydown',(e)=>{
-    if (e.keyCode === 38) {
-        keyboard.up = true;
+const keyboardHandler=(event)=>{
+    if (keyboard.hasOwnProperty(event.code)){
+        keyboard[event.code]=event.type==='keydown';
     }
-    if (e.keyCode === 40) {
-        keyboard.down = true;
-    }
-    if (e.keyCode === 37) {
-        keyboard.left = true;
-    }
-    if (e.keyCode === 39) {
-        keyboard.right = true;
-    }
-    if (e.keyCode === 87) {
-        keyboard.keyW = true;
-    }
-    if (e.keyCode === 65) {
-        keyboard.keyA = true;
-    }
-    if (e.keyCode === 83) {
-        keyboard.keyS = true;
-    }
-    if (e.keyCode === 68) {
-        keyboard.keyD = true;
-    }
-});
+};
 
-addEventListener('keyup',(e)=>{
-    if (e.keyCode === 38) {
-        keyboard.up = false;
-    }
-    if (e.keyCode === 40) {
-        keyboard.down = false;
-    }
-    if (e.keyCode === 37) {
-        keyboard.left = false;
-    }
-    if (e.keyCode === 39) {
-        keyboard.right = false;
-    }
-    if (e.keyCode === 87) {
-        keyboard.keyW = false;
-    }
-    if (e.keyCode === 65) {
-        keyboard.keyA = false;
-    }
-    if (e.keyCode === 83) {
-        keyboard.keyS = false;
-    }
-    if (e.keyCode === 68) {
-        keyboard.keyD = false;
-    }
-});
+addEventListener('keydown',keyboardHandler);
+addEventListener('keyup',keyboardHandler);
 
-addEventListener('mousedown',(e)=>{
-   if (e.which===1){
-       mouse.isLeftClicked=true;
-   }
-   if (e.which===3){
-       mouse.isRightClicked=true;
-   }
-   mouse.clickPosition.set(e.clientX,e.clientY);
-});
+const mouseHandler=(event)=>{
+    const state=event.type==='mousedown';
+    if (event.which===1){
+        mouse.isLeftClicked=state;
+    }
+    if (event.which===3){
+        mouse.isRightClicked=state;
+    }
+    if (state){
+        mouse.clickPosition.set(event.clientX,event.clientY);
+    }
+};
 
-addEventListener('mouseup',(e)=>{
-    if (e.which===1){
-        mouse.isLeftClicked=false;
-    }
-    if (e.which===3){
-        mouse.isRightClicked=false;
-    }
-});
+addEventListener('mousedown',mouseHandler);
+addEventListener('mouseup',mouseHandler);
 
