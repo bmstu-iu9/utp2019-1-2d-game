9740530d213271e876858e132ce27f1a78d62c02
FloryaVictor <lapunixa@yandex.ru>
2019-08-05 19:38:56 +0300

    #10 Realize PlayerManager

--- js/NPC.js
+++ js/NPC.js
@@ -1,3 +1,8 @@
+'use strict'
+
+/**
+ * Game Player
+ */
 class NPC extends GameObject {
     /**
      * 
@@ -8,29 +13,14 @@ class NPC extends GameObject {
         super(id)
         this.actor = new MovableActor(coords, centre)
         this.drawable = new DrawableObject("middleground", SpriteFactory.CreateTestSprite())
+        this.manager=new PlayerManager(this)
     }
+
     render() {
         this.drawable.render()
     }
 
     Update() {
-        let speed = 10
-        if (keyboard.KeyW) {
-            this.actor.changePosition(new Vector2d(0, -speed))
-            //console.log(this.actor.position)
-        }
-        if (keyboard.KeyS) {
-            this.actor.changePosition(new Vector2d(0, speed))
-            //console.log(this.actor.position)
-        }
-        if (keyboard.KeyA) {
-            this.actor.changePosition(new Vector2d(-speed, 0))
-            //console.log(this.actor.position)
-        }
-        if (keyboard.KeyD) {
-            this.actor.changePosition(new Vector2d(speed, 0))
-            //console.log(this.actor.position)
-        }
-        Game.camera.focusOn(this.actor.position)
+        this.manager.update()
     }
 }
