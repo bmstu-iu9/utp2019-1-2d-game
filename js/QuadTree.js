7811845355ebc4f420a2d71e129785d9323ed670
vlad333rrty <vlad333rrty90@mail.ru>
2019-08-10 22:03:38 +0300

    Quadtree retrieve method improved

--- js/QuadTree.js
+++ js/QuadTree.js
@@ -10,21 +10,41 @@ class Rectangle{
 
 
 class QuadTree{
-    constructor(bounds,capacity){
+    constructor(bounds,capacity,parent){
         this.bounds=bounds;
         this.next=new Array(4).fill(null);
         this.objects=[];
-        this.capacity=capacity;
+        this.capacity=capacity||4;
+        this.parent=parent||this;
     }
 
     divide(){
         const halfHeight=~~this.bounds.height/2;
         const halfWidth=~~this.bounds.width/2;
 
-        this.next[0]=new QuadTree(new Rectangle(this.bounds.x+halfWidth,this.bounds.y,halfWidth,halfHeight),this.capacity);
-        this.next[1]=new QuadTree(new Rectangle(this.bounds.x,this.bounds.y,halfWidth,halfHeight),this.capacity);
-        this.next[2]=new QuadTree(new Rectangle(this.bounds.x,this.bounds.y+halfHeight,halfWidth,halfHeight),this.capacity);
-        this.next[3]=new QuadTree(new Rectangle(this.bounds.x+halfWidth,this.bounds.y+halfHeight,halfWidth,halfHeight),this.capacity);
+        this.next[0]=new QuadTree(new Rectangle(this.bounds.x+halfWidth,this.bounds.y,halfWidth,halfHeight),this.capacity,this);
+        this.next[1]=new QuadTree(new Rectangle(this.bounds.x,this.bounds.y,halfWidth,halfHeight),this.capacity,this);
+        this.next[2]=new QuadTree(new Rectangle(this.bounds.x,this.bounds.y+halfHeight,halfWidth,halfHeight),this.capacity,this);
+        this.next[3]=new QuadTree(new Rectangle(this.bounds.x+halfWidth,this.bounds.y+halfHeight,halfWidth,halfHeight),this.capacity,this);
+    }
+
+    /**
+     *
+     * @param {AABB,CircleHitbox} object
+     * @returns {{left: boolean, right: boolean, up: boolean, down: boolean}}
+     */
+    getEstimation(object){
+        const minMaxY=object.getMinMax('y');
+        const minMaxX=object.getMinMax('x');
+
+        const xCentre=this.bounds.x+(~~this.bounds.width/2);
+        const yCentre=this.bounds.y+(~~this.bounds.height/2);
+        return{
+            right:minMaxX.min>xCentre,
+            left:minMaxX.max<xCentre,
+            up:minMaxY.max<yCentre,
+            down:minMaxY.min>yCentre
+        }
     }
 
     /**
@@ -33,39 +53,35 @@ class QuadTree{
     getIndex(object){
         let index;
 
-        const minMaxY=object.getMinMax('y');
-        const minMaxX=object.getMinMax('x');
+        const estimation=this.getEstimation(object);
 
-        const xCentre=this.bounds.x+(~~this.bounds.width/2);
-        const yCentre=this.bounds.y+(~~this.bounds.height/2);
-
-        const right=minMaxX.min>xCentre;
-        const left=minMaxX.max<xCentre;
-        const up=minMaxY.max<yCentre;
-        const down=minMaxY.min>yCentre;
-        if (right){
-            if (up){
+        if (estimation.right){
+            if (estimation.up){
                 index=0;
             }
-            if (down){
+            if (estimation.down){
                 index=3;
             }
-        }else if (left){
-            if (up){
+        }else if (estimation.left){
+            if (estimation.up){
                 index=1;
             }
-            if (down){
+            if (estimation.down){
                 index=2;
             }
         }
-        return index;
+        return {
+            index:index,
+            estimation:estimation
+        };
     }
 
     add(object){
         if (this.next[0]!=null){
             const index=this.getIndex(object);
-            if (index!==undefined)
-                this.next[index].add(object);
+            if (index.index!==undefined){
+                this.next[index.index].add(object);
+            }
             else this.objects.push(object);
         }else {
             this.objects.push(object);
@@ -73,14 +89,29 @@ class QuadTree{
                 this.divide();
                 for (let i=0;i<this.objects.length;){
                     const index=this.getIndex(this.objects[i]);
-                    if (index!==undefined){
-                        this.next[index].add(this.objects.splice(i,1)[0]);
+                    if (index.index!==undefined){
+                        this.next[index.index].add(this.objects.splice(i,1)[0]);
                     }else {
                         i++;
                     }
                 }
             }
         }
+    }
+
+
+    static getIndexes(estimation){
+        if (!estimation.up && !estimation.down){
+            if (!estimation.right && !estimation.left){
+                return [0,1,2,3]
+            }
+            if (estimation.right) return [0,3]
+            if (estimation.left) return [1,2]
+        }else if (!estimation.right && !estimation.left){
+            if (estimation.up) return [0,1]
+            if (estimation.down) return [2,3]
+        }
+        return [0,1,2,3]
     }
 
     /**
@@ -90,11 +121,11 @@ class QuadTree{
     retrieve(objects,object){
         if (this.next[0]!=null){
             const index=this.getIndex(object);
-            if (index!==undefined){
-                this.next[index].retrieve(objects,object);
+            if (index.index!==undefined){
+                this.next[index.index].retrieve(objects,object);
             }else {
-                for (let n of this.next){
-                    n.retrieve(objects,object);
+                for (let i of QuadTree.getIndexes(index.estimation)){
+                    this.next[i].retrieve(objects,object);
                 }
             }
         }
@@ -111,5 +142,4 @@ class QuadTree{
             }
         }
     }
-
 }
