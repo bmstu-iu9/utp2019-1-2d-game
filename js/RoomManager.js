158fbec31a4e730fc32932640e7f1e124d8aac79
FloryaVictor <lapunixa@yandex.ru>
2019-08-12 00:04:21 +0300

    bug fix

--- js/RoomManager.js
+++ js/RoomManager.js
@@ -13,22 +13,31 @@ class RoomManager {
     }
 
     /**
+     *Обновляет позицию объекта на тайловой карте
+     * @param obj
+     */
+    refreshPosition(obj){
+        if (obj.drawable.placement === "middleground") {
+            let prevI = ~~(obj.actor.prevPosition.y / Game.tileHeight)
+            let prevJ = ~~(obj.actor.prevPosition.x / Game.tileWidth)
+            let i = ~~(obj.actor.position.y / Game.tileHeight)
+            let j = ~~(obj.actor.position.x / Game.tileWidth)
+            if ((prevI !== i || prevJ !== j) && !this.managableObj.middlegroundTiles[i][j].hasId(obj)) {
+                this.managableObj.middlegroundTiles[prevI][prevJ].delete(obj)
+                this.managableObj.middlegroundTiles[i][j].set(obj)
+            }
+        }
+    }
+
+
+    /**
      * Обновляет все игровые объекты внури managebleObj (Room)
      * и перемещает их на тайловой карте,если нужно
      */
     Update() {
         this.managableObj.updatableObjects.forEach(element => {
+            this.refreshPosition(element)
             element.Update();
-            if (element.drawable.placement === "middleground") {
-                let prevI = ~~(element.actor.prevPosition.y / Game.tileHeight)
-                let prevJ = ~~(element.actor.prevPosition.x / Game.tileWidth)
-                let i = ~~(element.actor.position.y / Game.tileHeight)
-                let j = ~~(element.actor.position.x / Game.tileWidth)
-                if ((prevI !== i || prevJ !== j) && !this.managableObj.middlegroundTiles[i][j].hasId(element)) {
-                    this.managableObj.middlegroundTiles[prevI][prevJ].delete(element)
-                    this.managableObj.middlegroundTiles[i][j].set(element)
-                }
-            }
         });
     }
 
