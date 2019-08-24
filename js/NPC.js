f50d700bcf5b08b9ea812192f526c31d952caf61
FloryaVictor <lapunixa@yandex.ru>
2019-08-25 02:07:04 +0300

    #18 Hitbox auto configuration

--- js/NPC.js
+++ js/NPC.js
@@ -8,15 +8,17 @@ class NPC extends GameObject {
      *
      * @param {Vector2d} [coords = new Vector2d(0,0)]
      * @param {Vector2d} [centre = new Vector2d(0,0)]
-     * @param manager
+     * @param {DrawableObject} drawable
+     * @param hitbox
      * @param id
+     * @param manager
      */
-    constructor(coords = new Vector2d(0, 0), centre = new Vector2d(0, 0), manager = undefined, id = Game.getUniqId()) {
-        super(id);
-        this.actor = new MovableActor(coords, centre);
-        this.drawable = new DrawableObject("middleground", SpriteFactory.CreateTestSprite());
+    constructor(coords = new Vector2d(0, 0), centre = new Vector2d(0, 0), drawable, hitbox, id = Game.getUniqId(), manager = undefined) {
+        super(id)
+        this.actor = new MovableActor(coords, centre)
+        this.drawable = drawable
+        this.hitbox = hitbox
         this.manager = manager
-        this.hitbox = new Hitbox(HITBOX_CIRCLE, new Vector2d(centre), 26);
     }
 
     render() {
@@ -31,18 +33,18 @@ class NPC extends GameObject {
         this.hitbox.update(this.actor.centre)
     }
 
-    onCollide(collision){
-       this.hitbox.correctPosition(collision);
-       this.actor.changePosition(collision.distance);
+    onCollide(collision) {
+        this.hitbox.correctPosition(collision);
+        this.actor.changePosition(collision.distance);
     }
 
-    toJSON(){
+    toJSON() {
         return {
-            id : this.id,
-            actor : this.actor,
-            drawable : this.drawable,
-            hitbox : this.hitbox,
-            manager : this.manager !== undefined
+            id: this.id,
+            actor: this.actor,
+            drawable: this.drawable,
+            hitbox: this.hitbox,
+            manager: this.manager !== undefined
         }
     }
 
@@ -50,17 +52,16 @@ class NPC extends GameObject {
      *
      * @param {NPC} object
      */
-    static fromJSON(object){
+    static fromJSON(object) {
         let npc
-        if (object.manager === true){
+        if (object.manager === true) {
             npc = TilesFactory.CreatePlayer()
             npc.id = object.id
             npc.actor = MovableActor.fromJSON(object.actor)
             npc.hitbox = Hitbox.fromJSON(object.hitbox)
             Game.camera.focusOn(npc.actor)
-        }
-        else {
-            npc = TilesFactory.CreateStaticNPC(0,0)
+        } else {
+            npc = TilesFactory.CreateStaticNPC(0, 0)
             npc.id = object.id
             npc.actor = MovableActor.fromJSON(object.actor)
             npc.hitbox = Hitbox.fromJSON(object.hitbox)
--- js/TilesFactory.js
+++ js/TilesFactory.js
@@ -1,29 +1,31 @@
 'use strict';
+
 class TilesFactory {
 
     /**
      *
      * @param {Image} img
      */
-    static CreateTexture(img){
+    static CreateTexture(img) {
         let obj = new Texture(img);
         textureStorage[img.src] = obj
         return obj
     }
+
     /**
-     * 
-     * @param {Image} img 
+     *
+     * @param {Image} img
      */
-    static CreateTexture(img){
+    static CreateTexture(img) {
         let obj = new Texture(img);
         textureStorage[img.src] = obj
         return obj
-    }   
+    }
 
     /**
      * Порождает и возвращает тайл по заданным игровым координатам
-     * @param {Number} x 
-     * @param {Number} y 
+     * @param {Number} x
+     * @param {Number} y
      */
     static CreateTestTile(x = 0, y = 0) {
         return new StaticObject(x, y, x, y, new DrawableObject("background", Game.BrickTexture));
@@ -31,37 +33,74 @@ class TilesFactory {
 
     /**
      * Порождает и Player по заданным игровым координатам
-     * @param {Number} x 
-     * @param {Number} y 
+     * @param {Number} x
+     * @param {Number} y
      */
     static CreatePlayer(x = 0, y = 0) {
-        let player = new NPC(new Vector2d(x, y), new Vector2d(x + 25, y + 25));
+        let player = TilesFactory.createNPC(x, y, 26, new DrawableObject("middleground", SpriteFactory.CreateTestSprite()));
         player.manager = new PlayerManager(player)
         return player
     }
 
     /**
      * Порождает и NPC по заданным игровым координатам
-     * @param {Number} x 
-     * @param {Number} y 
+     * @param {Number} x
+     * @param {Number} y
      */
     static CreateStaticNPC(x, y) {
-        return new NPC(new Vector2d(x, y), new Vector2d(x + 25, y + 25))
+        return TilesFactory.createNPC(x, y, 26, new DrawableObject("middleground", SpriteFactory.CreateTestSprite()))
     }
 
     /**
      * Порождает и возвращает тайл по заданным игровым координатам
-     * @param {Number} x 
-     * @param {Number} y 
+     * @param {Number} x
+     * @param {Number} y
      */
     static CreateTestGrassTile(x = 0, y = 0) {
-        let tile = new StaticObject(100, 100, 125, 125, new DrawableObject("middleground", Game.GrassTexture));
+        return TilesFactory.createTile(100, 100, 50, 50, new DrawableObject("middleground", Game.GrassTexture));
+    }
+
+    /**
+     * create tile without hitbox
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     * @param {DrawableObject} drawable
+     * @returns {StaticObject}
+     */
+    static createTile(x = 0, y = 0, w = 0, h = 0, drawable) {
+        let height = drawable.drowable.height
+        let width = drawable.drowable.width
+        let xCentre = ~~(x + width - w / 2)
+        let yCentre = ~~(y + height - h / 2)
+        let tile = new StaticObject(x, y, xCentre, yCentre, drawable)
         tile.hitbox = new AABB(new Vector2d(tile.actor.centre), [
             tile.actor.centre.add(-25, -25, new Vector2d()),
             tile.actor.centre.add(25, -25, new Vector2d()),
-            tile.actor.centre.add(25, 40, new Vector2d()),
-            tile.actor.centre.add(-25, 40, new Vector2d())
-        ]);
+            tile.actor.centre.add(25, 25, new Vector2d()),
+            tile.actor.centre.add(-25, 25, new Vector2d())
+        ])
         return tile
+    }
+
+    /**
+     * create NPC with hitbox
+     * @param x
+     * @param y
+     * @param r
+     * @param {DrawableObject} drawable
+     * @returns {NPC}
+     */
+    static createNPC(x = 0, y = 0, r = 0, drawable) {
+        let height = drawable.drowable.height
+        let width = drawable.drowable.width
+        let xCentre = ~~(x + width / 2)
+        let yCentre = ~~(y + height - r)
+        return new NPC(new Vector2d(x, y),
+            new Vector2d(xCentre, yCentre),
+            drawable,
+            new Hitbox(HITBOX_CIRCLE, new Vector2d(xCentre, yCentre), r)
+        )
     }
 }
