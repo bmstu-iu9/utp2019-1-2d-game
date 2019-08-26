88f6b13840779b1a8e94cb566f04994d94b4b562
FloryaVictor <lapunixa@yandex.ru>
2019-08-26 20:37:32 +0300

    ability class added

---
+++ js/Ability.js
@@ -0,0 +1,11 @@
+class Ability{
+    constructor(manaCost, coolDown, castTime){
+        this.manaCost = manaCost
+        this.coolDown = coolDown
+        this.castTime = castTime
+    }
+
+    cast(){
+        
+    }
+}
