92570cc3cfebe65d9bed431a5090df928e0e0007
FloryaVictor <lapunixa@yandex.ru>
2019-08-26 20:15:03 +0300

    tile's hitbox alignment added

--- js/TilesFactory.js
+++ js/TilesFactory.js
@@ -51,28 +51,79 @@ class TilesFactory {
     }
 
     /**
-     * create tile without hitbox
+     * create tile with hitbox
      * @param x
      * @param y
      * @param w
      * @param h
      * @param {DrawableObject} drawable
+     * @param {String} alignment Options : LeftTop, LeftBot, RightTop, RightBot, Center, Bot
      * @returns {StaticObject}
      */
-    static createTile(x = 0, y = 0, w = Game.tileWidth, h = Game.tileHeight, drawable) {
-        if(drawable.placement !== "middleground")
+    static createTile(x = 0, y = 0, w = Game.tileWidth, h = Game.tileHeight, drawable, alignment) {
+        if (drawable.placement !== "middleground")
             throw "Solid object placement can be middleground only"
         let height = drawable.drowable.height
         let width = drawable.drowable.width
-        let xCentre = ~~(x + width / 2)
-        let yCentre = ~~(y + height - h / 2)
+        let xCentre, yCentre
+        let lt = new Vector2d()
+        let rt = new Vector2d()
+        let rb = new Vector2d()
+        let lb = new Vector2d()
+        switch (alignment) {
+            case 'Bot':
+                xCentre = ~~(x + width / 2)
+                yCentre = ~~(y + height - h / 2)
+                lt.set(~~(x + width / 2 - w / 2), y + height - h)
+                rt.set(~~(x + width / 2 + w / 2), y + height - h)
+                rb.set(~~(x + width / 2 + w / 2), y + height)
+                lb.set(~~(x + width / 2 - w / 2), y + height)
+                break
+            case 'Center':
+                xCentre = ~~(x + width / 2)
+                yCentre = ~~(y + height / 2)
+                lt.set(~~(x + width / 2 - w / 2), ~~(y + height / 2 - h / 2))
+                rt.set(~~(x + width / 2 + w / 2), ~~(y + height / 2 - h / 2))
+                rb.set(~~(x + width / 2 + w / 2), ~~(y + height / 2 + h / 2))
+                lb.set(~~(x + width / 2 - w / 2), ~~(y + height / 2 + h / 2))
+                break
+            case 'LeftTop':
+                xCentre = ~~(x + w / 2)
+                yCentre = ~~(y + h / 2)
+                lt.set(x, y)
+                rt.set(x + w, y)
+                rb.set(x + w, y + h)
+                lb.set(x, y + h)
+                break
+            case 'LeftBot':
+                xCentre = ~~(x + w / 2)
+                yCentre = ~~(y + height - h / 2)
+                lt.set(x, y + height - h)
+                rt.set(x + w, y + height - h)
+                rb.set(x + w, y + height)
+                lb.set(x, y + height)
+                break
+            case 'RightTop':
+                xCentre = ~~(x + width - w / 2)
+                yCentre = ~~(y + h / 2)
+                lt.set(x + width - w, y)
+                rt.set(x + width, y)
+                rb.set(x + width, y + h)
+                lb.set(x + width - w, y + h)
+                break
+            case 'RightBot':
+                xCentre = ~~(x + width - w / 2)
+                yCentre = ~~(y + height - h / 2)
+                lt.set(x + width - w, y + height - h)
+                rt.set(x + width, y + height - h)
+                rb.set(x + width, y + height)
+                lb.set(x + width - w, y + height)
+                break
+            default:
+                throw "wrong alignment type"
+        }
         let tile = new StaticObject(x, y, xCentre, yCentre, drawable)
-        tile.hitbox = new AABB(new Vector2d(tile.actor.centre), [
-            new Vector2d(~~(x + width / 2 - w / 2), y + height - h),
-            new Vector2d(~~(x + width / 2 + w / 2), y + height - h),
-            new Vector2d(~~(x + width / 2 + w / 2), y + height),
-            new Vector2d(~~(x + width / 2 - w / 2), y + height)
-        ])
+        tile.hitbox = new AABB(new Vector2d(tile.actor.centre), [lt, rt, rb, lb])
         return tile
     }
 
@@ -85,7 +136,7 @@ class TilesFactory {
      * @returns {NPC}
      */
     static createNPC(x = 0, y = 0, r = ~~(Game.tileWidth / 2), drawable) {
-        if(drawable.placement !== "middleground")
+        if (drawable.placement !== "middleground")
             throw "Solid object placement can be middleground only"
         let height = drawable.drowable.height
         let width = drawable.drowable.width
