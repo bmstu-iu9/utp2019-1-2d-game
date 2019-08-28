ec37bff5833ce86ce54978a4ed25b098e25b278b
Alexey Tarasov <tarasovalx99@gmail.com>
2019-08-28 03:24:03 +0300

    Hero idle animation fix. Casting animation added

--- js/BoxFactory.js
+++ js/BoxFactory.js
@@ -10,7 +10,7 @@ class BoxFactory {
     }
     static CreateKnightBox() {
         let idle = [new SpritePattern("knight_idle_right", imagesStorage.knight, [0], "horizontal", 0, 3, 84, 84, false, 1),
-        new SpritePattern("knight_sword_top", imagesStorage.knight, [0, 1, 2], "horizontal", 6, 3, 84, 84, false, 3),
+        new SpritePattern("knight_idle_top", imagesStorage.knight, [0], "horizontal", 9, 2, 84, 84, false, 3),
         new SpritePattern("knight_idle_left", imagesStorage.knight, [0], "horizontal", 0, 2, 84, 84, false, 1),
         new SpritePattern("knight_idle_bot", imagesStorage.knight, [0, 1, 2, 3], "horizontal", 0, 0, 84, 84, false, 1)]
         let beat = [new SpritePattern("knight_sword_right", imagesStorage.knight, [0, 1, 2], "horizontal", 4, 0, 84, 84, true, 3),
@@ -21,6 +21,10 @@ class BoxFactory {
         new SpritePattern("knight_run_top", imagesStorage.knight, [0, 1, 2, 3, 4], "horizontal", 5, 1, 84, 84, false, 7),
         new SpritePattern("knight_run_left", imagesStorage.knight, [5, 4, 2, 1], "horizontal", 0, 2, 84, 84, false, 7),
         new SpritePattern("knight_run_bot", imagesStorage.knight, [0, 1, 2, 3, 4], "horizontal", 0, 1, 84, 84, false, 7)]
+        let cast = [new SpritePattern("knight_cast_right", imagesStorage.knight, [0, 1, 2], "horizontal", 0, 4, 84, 84, false, 7),
+        new SpritePattern("knight_cast_top", imagesStorage.knight, [0, 1, 2], "horizontal", 0, 5, 84, 84, false, 7),
+        new SpritePattern("knight_cast_left", imagesStorage.knight, [0, 1, 2], "horizontal", 3, 4, 84, 84, false, 7),
+        new SpritePattern("knight_cast_bot", imagesStorage.knight, [0, 1, 2], "horizontal", 6, 4, 84, 84, false, 7)]
         let result = new SpriteBox()
         idle.forEach((u) => {
             result.add("idle", u)
@@ -31,12 +35,15 @@ class BoxFactory {
         go.forEach((u) => {
             result.add("go", u)
         })
+        cast.forEach((u) => {
+            result.add("cast", u)
+        })
         return result
     }
 
     static CreateFireBallBox() {
-        let seq = Array(61).fill(1).map((e,i) => i)
-        let fly = new SpritePattern('fireball_fly_bot', imagesStorage.fire_ball, seq, "horizontal", 0, 0, 52, 52, false, 2, -Math.PI/2)
+        let seq = Array(61).fill(1).map((e, i) => i)
+        let fly = new SpritePattern('fireball_fly_bot', imagesStorage.fire_ball, seq, "horizontal", 0, 0, 52, 52, false, 2, -Math.PI / 2)
         let result = new SpriteBox()
         result.add("fly", fly)
         return result
